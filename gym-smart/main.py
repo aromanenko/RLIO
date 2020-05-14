af0598c139277104b5ac5a89f40203c29fd29fd3
HerrMorozovDmitry <31880478+Erlich-Heusmann@users.noreply.github.com>
2020-05-14 17:14:41 +0300

    Update main.py

--- gym-smart/main.py
+++ gym-smart/main.py
@@ -2,5 +2,11 @@ import gym
 import gym_smart
 
 env = gym.make('SmartEnv-v0')
-env.step()
-env.reset()
+env.load_data('/Users/fritzwilliams/Google Диск/A Reinforcement Learning Approach for Inventory Optimization in Retail/MERGE_TABLE_STORE_4600/MERGE_TABLE_STORE_4600.csv',
+              '/Users/fritzwilliams/Google Диск/A Reinforcement Learning Approach for Inventory Optimization in Retail/MERGE_TABLE_STORE_4600/data/echelon/echelon_1_sl.csv',
+              '/Users/fritzwilliams/Google Диск/A Reinforcement Learning Approach for Inventory Optimization in Retail/MERGE_TABLE_STORE_4600/demand_data_train.csv'
+              )
+print('load_data DONE!')
+env.initial_state(4600, 25100)
+# env.step()
+# env.reset()
