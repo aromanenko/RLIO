a27921ea8455073dfcf3e8ddfe08c604450a7a4c
HerrMorozovDmitry <31880478+Erlich-Heusmann@users.noreply.github.com>
2020-06-06 15:23:06 +0300

    Little changes

--- gym-smart/main.py
+++ gym-smart/main.py
@@ -11,24 +11,33 @@ env.load_pairs('/Users/fritzwilliams/Desktop/A Reinforcement Learning Approach f
 
 # env.initial_state(4600, 25100)
 # env.initial_demand(4600, 25100, 1)
-
 # pairs_data, _ = env.calculate_states()
-# print(pairs_data.iloc[:2])
-
 # demand_data = env.calculate_demand(1)
-# print(demand_data.iloc[:2])
 
-# env.learn(2, 10, 0.01, 0.1, 1, 0.3)
-env.learn_single(4600, 386400, 10, 0.01, 0.1, 1, 0.3)
+shop_id = 4600
+product_id = 386400
+max_steps = 10
+alpha = 0.01
+probability = 0.1
+LT = 1
+alpha_order = 0.3
+
+# env.learn(2, max_steps, alpha, probability, LT, alpha_order)
+env.learn_single(shop_id, product_id, max_steps, alpha, probability, LT, alpha_order)
 
 state_data, action_data, env_data = env.reset()
 
-obs = pd.DataFrame([[4600, 386400, 5, 15, 0.95, 0]], columns=['location', 'sku', 'sales', 'stock', 'sl', 'order'])
+sales = 5
+stock = 15
+service_level = 0.95
+order = 0
+
+obs = pd.DataFrame([[shop_id, product_id, sales, stock, service_level, order]], columns=['location', 'sku', 'sales', 'stock', 'sl', 'order'])
 print('Initial State:\n', obs)
 
 for i in range(5):
     action = env.predict(obs)
-    obs, rew = env.step(obs, action, 1, 0.3)
+    obs, rew = env.step(obs, action, LT, alpha_order)
     env.render(action, obs, rew)
 
 final_rew = env.reward(obs)
